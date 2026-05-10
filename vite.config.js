3ad98341c3a981729b161a5f52c9ae05087f19ae
Anonymous_947 <evansogala831@gmail.com>
2026-05-10 11:15:44 +0100

    restore react project structure

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  base: "/evansogala.github.io/"
-}
+export default defineConfig({
+  plugins: [react()],
+  base: '/'
+})
